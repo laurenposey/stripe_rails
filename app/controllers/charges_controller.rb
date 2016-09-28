b944ed05311d0eab65076ce5007a579e475cb4ed
Brian Grant and Lauren Posey <brian+lauren@>
2016-09-28 13:55:54 -0700

    stripe fully implemented

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,28 @@
 class ChargesController < ApplicationController
   def new
-end
+    @cart = current_cart
+    @amount = @cart.products.sum(:price)
+  end
 
-def create
-  # Amount in cents
-  @amount = 500
+  def create
+    # Amount in cents
+    @cart = current_cart
+    @amount = @cart.products.sum(:price)
 
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
-  )
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
-end
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :products
   resources :carts
+  resources :charges
 end
